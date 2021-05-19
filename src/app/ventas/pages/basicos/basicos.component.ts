3a9aa226cc772d8bfc54dbaf12327ea63d298504
Theo wald <theo83wald@gmail.com>
2021-05-19 18:08:02 +0200

    fin_19_05_2021

--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
-  constructor() { }
+  nombreLower: string = 'juan lu';
+  nombreUpper: string = 'JUAN LU';
+  nombreCompleto: string = 'jUaN luiS arMAs';
 
-  ngOnInit(): void {
-  }
+  fecha: Date = new Date();
 
 }
