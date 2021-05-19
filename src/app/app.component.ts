ced7c4aba5bdb988759b8f23dcb9e47df31c738a
Theo wald <theo83wald@gmail.com>
2021-05-19 14:38:21 +0200

    primer commit pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  
+  nombre: string = 'Juan lu aRmas';
+
+  mostrarNombre(){
+    console.log(this.nombre);
+  }
+
 }
